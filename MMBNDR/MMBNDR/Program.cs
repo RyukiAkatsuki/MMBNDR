6dd195445b46502c641452b7ec8e5663695a342c
R_K <shawnfroste@outlook.fr>
2023-10-06 20:05:33 +0200

    Add Program Advance

--- MMBNDR/MMBNDR/Program.cs
+++ MMBNDR/MMBNDR/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 
- class Program
+class Program
  {
     static void Main()
     {
@@ -12,12 +13,34 @@
         Enemy enemy = new Enemy("Mettaur", 40, 10);
 
         // Create Chip
-        Chip chip1 = new Chip("Cannon", 40, "A Simple Cannon", "A");
-        Chip chip2 = new Chip("Sword", 80, "A Simple Sword", "S");
+        Chip chip1 = new Chip("Cannon", 40, "A Simple Cannon", "C");
+        Chip chip2 = new Chip("HiCannon", 80, "A Normal Cannon", "H");
+        Chip chip3 = new Chip("MCannon", 120, "A Stronger Cannon", "M");
 
+        // Create Program Advance
+        ProgramAdvance programAdvance1 = new ProgramAdvance(
+            "OmegaCanon",
+            new List<string> {"C", "H", "M"},
+            300,
+            "Powerful combinaison with all Cannon"
+            );
+
+        List<Chip> playerChips = new List<Chip> { chip1, chip2, chip3 };
+        if (programAdvance1.CanExecute(playerChips))
+        {
+            // Execute the Program Advance
+            Console.WriteLine($"Executing {programAdvance1.Name}!");
+            int damageDealt = programAdvance1.Damage;
+            Console.WriteLine($"Dealt {damageDealt} damage to the enemy.");
+        }
+        else
+        {
+            Console.WriteLine($"Cannot execute {programAdvance1.Name}. Missing required chips.");
+        }
         // Add Chip
         player.AddChip(chip1);
         player.AddChip(chip2);
+        player.AddChip(chip3);
 
 
     }
---
+++ MMBNDR/MMBNDR/ProgramAdvance.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ProgramAdvance
+{
+    public string Name { get; set; }
+    public List<string> RequiredChips { get; set; }
+    public int Damage { get; set; }
+    public string Description { get; set; }
+
+    public ProgramAdvance(string name, List<string> requiredChips, int damage, string description)
+    {
+        Name = name;
+        RequiredChips = requiredChips;
+        Damage = damage;
+        Description = description;
+    }
+
+    public bool CanExecute(List<Chip> playersChips)
+    {
+        // We check if the player has the required chips to execute the program advance
+        foreach (string requiredChipCode in RequiredChips)
+        {
+            bool found = false;
+            foreach(Chip chip in playersChips)
+            {
+                if (chip.ChipCode == requiredChipCode)
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+                return false; // Missing the required chip
+        }
+        return true; // All found
+    }
+}
