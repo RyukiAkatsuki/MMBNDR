d891ec9d569eea8c08638cca9b488b65dc4142d1
R_K <shawnfroste@outlook.fr>
2023-10-06 17:06:32 +0200

    Add ChipCode for Chip

--- MMBNDR/MMBNDR/Chip.cs
+++ MMBNDR/MMBNDR/Chip.cs
@@ -5,11 +5,13 @@ public class Chip
     public string Name { get; set; }
     public int Damage { get; set; }
     public string Description { get; set; }
+    public string ChipCode { get; set; }
 
-    public Chip(string name, int damage, string description)
+    public Chip(string name, int damage, string description, string chipCode)
     {
         Name = name;
         Damage = damage;
         Description = description;
+        ChipCode = chipCode;
     }
 }
--- MMBNDR/MMBNDR/Program.cs
+++ MMBNDR/MMBNDR/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
-namespace MMBNDR
-{
-    class Program
+ class Program
+ {
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+
+        // Player
+        Ace player = new Ace("Ace.EXE");
+
+        // Enemy
+        Enemy enemy = new Enemy("Mettaur", 40, 10);
+
+        // Create Chip
+        Chip chip1 = new Chip("Cannon", 40, "A Simple Cannon", "A");
+        Chip chip2 = new Chip("Sword", 80, "A Simple Sword", "S");
+
+        // Add Chip
+        player.AddChip(chip1);
+        player.AddChip(chip2);
+
+
     }
 }
