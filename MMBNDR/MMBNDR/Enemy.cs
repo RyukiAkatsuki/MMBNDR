edda31bbc8b81daf4867142219da416039a4cb32
R_K <shawnfroste@outlook.fr>
2023-10-10 00:11:08 +0200

    Add Items for Random drop

--- MMBNDR/MMBNDR/Enemy.cs
+++ MMBNDR/MMBNDR/Enemy.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-
+using System.Collections.Generic;
 public class Enemy
 {
     public string Name { get; set; }
     public int Health { get; set; }
     public int AttackPower { get; set; }
+    public List<Item> Drops { get; set; }
 
     public Enemy(string name, int initialHealth, int attackPower)
     {
@@ -19,5 +20,15 @@ public class Enemy
         Health -= damage;
         if (Health < 0)
             Health = 0;  // Ensure health doesn't go negative
+    }
+
+    public Item GetRandomDrop()
+    {
+        if (Drops.Count == 0)
+            return null;
+
+        Random random = new Random();
+        int randomIndex = random.Next(0, Drops.Count);
+        return Drops[randomIndex];
     }
 }
---
+++ MMBNDR/MMBNDR/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Item
+{
+	public string Name { get; set; }
+	public ItemType Type { get; set; }
+	public int Amount { get; set; }
+
+	public Item(string name, ItemType type, int amount)
+	{
+		Name = name;
+		Type = type;
+		Amount = amount;
+	}
+
+	public enum ItemType
+    {
+		Zenny,
+		BattleChip
+    }
+}
