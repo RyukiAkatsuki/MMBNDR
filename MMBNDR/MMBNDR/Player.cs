d8c8a41fc61f1bd906c8f4aa244a14377470a3e1
R_K <shawnfroste@outlook.fr>
2023-10-09 23:27:36 +0200

    Add Player

---
+++ MMBNDR/MMBNDR/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Player
+{
+    public string Name { get; set; }
+    public int HealthPoint { get; set; }
+    public int Zenny { get; set; }
+    public List<Chip> Chips { get; set; }
+    
+    public Player(string name)
+    {
+        Name = name;
+        HealthPoint = 100; // Initial HP
+        Zenny = 0; // Initial Money
+        Chips = new List<Chip>();
+    }
+
+    public void AddZenny(int amount)
+    {
+        Zenny += amount;
+    }
+
+    public void AddChip(Chip chip)
+    {
+        Chips.Add(chip);
+    }
+}
